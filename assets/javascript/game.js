dd7f569367746fd0b2850a7dc19a93093a22fae7
Cresence <devharris93@gmail.com>
2019-06-15 23:54:08 -0400

    [ver0.5] Wo~ah-oh, we're halfway there. Wo~ah-OH, livin' on a prayer! Ahem, anyway. Tons of conditional logic added. HTML elements redefined more logically. CSS elements made to be more consistent with sizing and positioning. JS file now has working character selector functionality, and random opposition as well. Visual representation of both available. HTML, CSS, and JS percentages until completion to be added with established battle mechanics in ver0.7 update. Stay tuned.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,91 @@
-var battleArea = $('.select-box');
-var usrArea = $('.bottom-box');
-var avatar = $('.btn');
-// 
-var char = [
-    {
-    class: 'knight',
-    health: 100,
-    attack: 45,
-    counter: 50
+// Phase 1 (Character Selection)
+$(document).ready(function(){
+    // Universal variable shortcut HTML elements
+    var battleArea = $('.select-box');
+    var usrArea = $('.bottom-box');
+    var avatar = $('.btn');
+    var inBattleArea = $('.misc-box');
+    var fightButton = $('#fight-btn');
+    var resetButton = $('#reset-btn');
+    // Characters defined w/ statistics as properties
+    var char = {
+        "Char1": {
+        class: "knight",
+        health: 100,
+        attack: 45,
+        counter: 50,
+        imageUrl: "./assets/images/knight.png"
     },
-    {
-    class: 'thief',
-    health: 75,
-    attack: 30,
-    counter: 70
+        "Char2": {
+        class: "thief",
+        health: 75,
+        attack: 30,
+        counter: 70,
+        imageUrl: "./assets/images/thief.png"
     },
-    {
-    class: 'sorcerer',
-    health: 35,
-    attack: 100,
-    counter: 100
-
+        "Char3": {
+        class: "sorcerer",
+        health: 40,
+        attack: 100,
+        counter: 100,
+        imageUrl: "./assets/images/sorcerer.png"
     }
-];
-var knight = char[0];
-var thief = char[1];
-var sorcerer = char[2];
-// 
-var selA = $('#pick1');
-var selB = $('#pick2');
-var selection = false;
-
-function gameStart(){
-$('.btn').on('click', function() {
-    var usrChoice = this;
-    $('#sel-area-text').text('You Picked:');
-    selA.append(usrChoice);
-    selection = true;
-    if (selection = true){
-        selB.append(usrChoice);
     };
-});
-};
+    // Battle area selection shortcuts
+    var selA = $('#pick1');
+    var selB = $('#pick2');
+    // Character Selection Guidelines
+    var selection = false;
+    var cpuChoice = 0;
+    // Hiding 2nd Phase (In-Battle)
+    fightButton.hide();
+    resetButton.hide();
+    battleArea.hide();
+    $('#sel-area-text').text('Choose Your Character');
+    // Phase 2 (Battle Stage)
+    function gameStart(){
+    $('.btn').on('click', function() {
+        var usrChoice = this.value;
+        // I'm still surprised that this took a long time to figure out, and that it even worked!
+        if (usrChoice < 2){
+            usrChoice = char.Char1;
+        }else if (usrChoice > 2) {
+            usrChoice = char.Char3;
+        }else {
+            usrChoice = char.Char2;
+        }
+        // End of previous contextual statement above referring to in-between code.
+        $('#sel-area-text').text('Begin!');
+        fightButton.show();
+        resetButton.show();
+        battleArea.show();
+        usrArea.hide();
+        selection = true;
+        charImage = $("<img alt='image' id='pick1'>").attr("src", usrChoice.imageUrl);
+        $(selA).append(charImage);
+        // 
+        if (selection = true) {
+            // CPU selection from array is then put into opposing side
+            cpuChoice = getMyEnemy(cpuChoice);
+            enemyImage = $("<img alt='image-2' id='pick2'>").attr("src", cpuChoice.imageUrl);
+            $(selB).append(enemyImage);
+            console.log(usrChoice);
+            console.log(cpuChoice);
+        }
+    });
+        // CPU auto selects from character pool
+        function getMyEnemy(cpuChoice) {
+        randomNumber = Math.floor((Math.random() * 3) + 1);
+        if (randomNumber < 2){
+            cpuChoice = char.Char1;
+        }else if (cpuChoice > 2) {
+            cpuChoice = char.Char3;
+        }else {
+            cpuChoice = char.Char2;
+        }
+        return cpuChoice;
+        };
+    };
 
-gameStart();
+    gameStart();
+});
